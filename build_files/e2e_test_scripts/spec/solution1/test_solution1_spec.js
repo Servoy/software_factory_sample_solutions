fb832a999d0defb83764d0d0c265c93f85ea28be
Andrei Costescu <costescuandrei@gmail.com>
2016-04-12 16:51:12 +0300

    Adapted e2e tests to small changes in solution.

--- build_files/e2e_test_scripts/spec/solution1/test_solution1_spec.js
+++ build_files/e2e_test_scripts/spec/solution1/test_solution1_spec.js
@@ -2,16 +2,16 @@ describe('test_hello_world', function() {
   it('should show say hello world', function() {
     var text, value, bool, source, url, title;
     var TestVars = {};
-    browser.get("http://localhost:8083/hello/solutions/hello/index.html");
+    browser.get("http://localhost:8083/solution1/solutions/solution1/index.html");
    	browser.wait(function() {
-		 return browser.isElementPresent(element(by.xpath("//*[@data-svy-name='world.mylabel']")));
+		 return browser.isElementPresent(element(by.xpath("//*[@data-svy-name='helloWorld.mylabel']")));
 	}, 80000);
-   	expect(element(by.buttonText('hello')).isPresent()).toBe(true);
-   	var value = element(by.xpath("//*[@data-svy-name='world.mylabel']")).getText();
-   	expect(value).toBe('world');
-   	element(by.xpath("//*[@data-svy-name='world.mybutton']")).click();
+   	expect(element(by.buttonText('Click me to change the label below')).isPresent()).toBe(true);
+   	var value = element(by.xpath("//*[@data-svy-name='helloWorld.mylabel']")).getText();
+   	expect(value).toBe('Hello world?');
+   	element(by.xpath("//*[@data-svy-name='helloWorld.mybutton']")).click();
     browser.waitForAngular();
-    value = element(by.xpath("//*[@data-svy-name='world.mylabel']")).getText();
-   	expect(value).toBe('world!');
+    value = element(by.xpath("//*[@data-svy-name='helloWorld.mylabel']")).getText();
+   	expect(value).toBe('Hello world!');
   });
 });
