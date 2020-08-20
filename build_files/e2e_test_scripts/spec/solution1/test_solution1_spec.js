d1f3e9e6816c538475e3a8cd8833d70cbb689fe0
Andrei Costescu <costescuandrei@gmail.com>
2020-08-20 17:40:15 +0300

    e2e.tomcat.base.url can be set in ant already

--- build_files/e2e_test_scripts/spec/solution1/test_solution1_spec.js
+++ build_files/e2e_test_scripts/spec/solution1/test_solution1_spec.js
@@ -1,8 +1,7 @@
 describe('test_hello_world', function() {
   it('should show say hello world', function() {
     var value;
-    var TestVars = {};
-    browser.get("http://lh:8083/solution1/solutions/solution1/index.html");
+    browser.get("/solution1/solutions/solution1/index.html");
    	browser.wait(function() {
 		 return browser.isElementPresent(element(by.xpath("//*[@data-svy-name='helloWorld.mylabel']")));
 	}, 80000);
