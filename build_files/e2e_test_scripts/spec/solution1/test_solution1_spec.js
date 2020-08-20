44c5e60ceea81ed2febf702c7371d3bb35320dae
Andrei Costescu <costescuandrei@gmail.com>
2020-08-20 17:09:34 +0300

    Just to check some SC setup.

--- build_files/e2e_test_scripts/spec/solution1/test_solution1_spec.js
+++ build_files/e2e_test_scripts/spec/solution1/test_solution1_spec.js
@@ -1,8 +1,8 @@
 describe('test_hello_world', function() {
   it('should show say hello world', function() {
-    var text, value, bool, source, url, title;
+    var value;
     var TestVars = {};
-    browser.get("http://localhost:8083/solution1/solutions/solution1/index.html");
+    browser.get("http://lh:8083/solution1/solutions/solution1/index.html");
    	browser.wait(function() {
 		 return browser.isElementPresent(element(by.xpath("//*[@data-svy-name='helloWorld.mylabel']")));
 	}, 80000);
